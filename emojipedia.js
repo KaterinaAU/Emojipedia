bbb7ecaa148d8613543e364793a89f58847a2d79
Katerina S <89135150+KaterinaAU@users.noreply.github.com>
2022-03-18 18:08:22 +1100

    Added files

---
+++ emojipedia.js
@@ -0,0 +1,44 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+  
+    {
+      id: 4,
+      emoji: "😅",
+      name: "Smiling Face With Open Mouth & Cold Swea",
+      meaning:"Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+    },
+    {
+       id: 5,
+       emoji: "🙃",
+       name:"Upside-Down Face",
+       meaning: "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+    },
+    {
+      id: 6,
+      emoji: "🤟",
+      name: "I Love You Gesture",
+      meaning: "In the American sign language, the fist with outstretched little finger, index finger and thumb means “I love you“. The ILY sign mainly conveys a general, positive message."
+    }
+  ];
+  
+  export default emojipedia;
